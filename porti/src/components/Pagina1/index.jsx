fb00918038331aef3dea3c9b8ed260b9ac60b5c6
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-07-16 00:44:25 -0300

    comeco da pagina 1

--- porti/src/components/Pagina1/index.jsx
+++ porti/src/components/Pagina1/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './styles.module.css';
+import avatar from '../../assets/ava.png'
+import arrowBottom from '../../assets/arrow-bottom.svg'
+import arrowTop from '../../assets/arrow-top.svg'
 
 const Pagina1 = () => {
     return (
       <div className={styles.Pagina1}>
-        <h1>Pagina 1</h1>
+        <div className={styles.header}> 
+            <div className={styles.palavras}>
+                <div className={styles.palavra}>Habilidades</div>
+                <div className={styles.palavra}>Portifólio</div>
+                <div className={styles.palavra}>Contato</div>
+            </div>
+        </div>
+
+        <div className={styles.corpo}>
+            <div className={styles.corpoTexto}>
+                <div className={styles.titulo}>
+                    <span>Olá, meu<br></br> nome é <strong className={styles.strong}>Julio</strong></span>
+                </div>
+                <div className={styles.subtitulo}>
+                    sou desenvolvedor full-stack
+                </div>
+                <div className={styles.botaoVerMais}>
+                    Ver mais
+                </div>
+            </div>
+            <div className={styles.corpoAvatar}>
+                <img src={avatar} alt="" className={styles.avatar}/>
+                <div className={styles.setas}>
+                    <img className={styles.seta} src={arrowTop} alt="" />
+                    <img className={styles.seta} src={arrowBottom} alt="" />
+                </div>
+            </div>
+        </div>
+
       </div>
     );
   };
