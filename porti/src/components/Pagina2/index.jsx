ad15b194fdc15114faf046857a4a71eba148ba1d
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-07-17 16:38:59 -0300

    conclusao dos elementos

--- porti/src/components/Pagina2/index.jsx
+++ porti/src/components/Pagina2/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './styles.module.css';
+import Icones from '../Icones'
 
 const Pagina2 = () => {
     return (
       <div className={styles.Pagina2}>
-        <h1>Pagina 2</h1>
+        <div className={styles.corpo}>
+            <div className={styles.corpoHabilidades}>
+              <div className={styles.titulo}>Habilidades</div>
+              <Icones/>
+            </div>
+            <div className={styles.corpoSobreMim}>
+              <div className={styles.titulo}>Sobre mim</div>
+              <div className={styles.texto}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto ea voluptas a fugit facilis pariatur, optio blanditiis vitae, id molestiae ipsam tenetur delectus maxime voluptatum! Magni error earum ut minus.
+              </div>
+            </div>
+        </div>
       </div>
     );
   };
