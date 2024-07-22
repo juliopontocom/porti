723cb970fc5ea3f746a300447b862f1ba58c60e7
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-07-22 17:33:17 -0300

    fixes botoes

--- porti/src/components/Pagina3/index.jsx
+++ porti/src/components/Pagina3/index.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 import { RxExternalLink } from "react-icons/rx";
 import { FaGithub } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 
+const projects = [
+  {
+    title: "Fábrica de promoções",
+    date: "28/07/2024",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam necessitatibus voluptatem praesentium quis, itaque adipisci corporis cum, alias, pariatur maxime sapiente ullam perspiciatis incidunt accusamus blanditiis quaerat. Dicta, ipsa facere.",
+    imageUrl: "../../assets/card1.png", // URL de exemplo para a imagem do projeto
+  },
+  {
+    title: "Projeto 2",
+    date: "15/08/2024",
+    description: "Descrição do Projeto 2. Nam necessitatibus voluptatem praesentium quis, itaque adipisci corporis cum, alias, pariatur maxime sapiente ullam perspiciatis incidunt accusamus blanditiis quaerat. Dicta, ipsa facere.",
+    imageUrl: "https://via.placeholder.com/600x400", // URL de exemplo para a imagem do projeto
+  },
+  {
+    title: "Projeto 3",
+    date: "10/09/2024",
+    description: "Descrição do Projeto 3. Nam necessitatibus voluptatem praesentium quis, itaque adipisci corporis cum, alias, pariatur maxime sapiente ullam perspiciatis incidunt accusamus blanditiis quaerat. Dicta, ipsa facere.",
+    imageUrl: "https://via.placeholder.com/600x400", // URL de exemplo para a imagem do projeto
+  },
+];
+
 const Pagina3 = () => {
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentProjectIndex((prevIndex) => (prevIndex + 1) % projects.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentProjectIndex((prevIndex) => (prevIndex - 1 + projects.length) % projects.length);
+  };
+
+  const currentProject = projects[currentProjectIndex];
+
   return (
     <div className={styles.Pagina3}>
       <div className={styles.corpoMaximo}>
         <div className={styles.titulo}>Portifólio</div>
-        <div className={styles.fotoProjeto}></div>
+        <div className={styles.fotoProjeto}>
+          <img src={currentProject.imageUrl} alt="" className={styles.imagemProjeto} />
+        </div>
         <div className={styles.descricaoProjeto}>
           <div className={styles.corpoCimaProjeto}>
-            <div className={styles.tituloProjeto}>Fábrica de promoções</div>
-            <div className={styles.dataProjeto}>28/07/2024</div>
+            <div className={styles.tituloProjeto}>{currentProject.title}</div>
+            <div className={styles.dataProjeto}>{currentProject.date}</div>
           </div>
           <div className={styles.textoProjeto}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam necessitatibus voluptatem praesentium quis, itaque adipisci corporis cum, alias, pariatur maxime sapiente ullam perspiciatis incidunt accusamus blanditiis quaerat. Dicta, ipsa facere.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, blanditiis odit nesciunt enim commodi, aperiam dicta reiciendis vitae temporibus voluptas facere, aliquid al
+            {currentProject.description}
           </div>
         </div>
         <div className={styles.botoes}>
@@ -33,11 +67,11 @@ const Pagina3 = () => {
             </div>
           </div>
           <div className={styles.botoesDireita}>
-            <div className={styles.botao}>
-            <FaArrowLeft className={styles.icone}/>
+            <div className={styles.botao} onClick={handlePrev}>
+              <FaArrowLeft className={styles.icone}/>
               Anterior
-              </div>
-            <div className={styles.botao}>
+            </div>
+            <div className={styles.botao} onClick={handleNext}>
               Próximo
               <FaArrowRight className={styles.icone}/>
             </div>
